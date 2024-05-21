e5d732fc0eba9f4db09bc02547549194cb599a10
ArturoAlvarezY <arturoavilamarketing@gmail.com>
2024-05-21 09:19:35 +0200

    complete the string methods exercised asigned in 20/05

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import { firstName } from "./js/exercise1.js";
 import { sum } from "./js/exercise2.js";
+import { showString } from "./js/exercise3.js";
 
 const app=()=>{
 console.log('my first name is'+ ' ' + firstName);
 console.log('the adition is' + ' ' + sum());
+console.log('the string is' + ' ' + evaluating('abcdenglish'));
 }
 
 
--- js/exercise3.js
+++ js/exercise3.js
@@ -1,7 +1,15 @@
-export let showString= 'English';
+export let showString;
 
 const evaluating=(str)=>{
 let minus;
 minus= str.toLowerCase();
-console.log(minus.includes('english', 'English'));
+console.log(minus.includes('english'));
 }
+
+console.log(evaluating('abcdenglish'));
+console.log(evaluating('sjdasdaenglish'));
+console.log(evaluating('sjdasdaenglsadgish'));
+console.log(evaluating('englishabckasjdsa'));
+console.log(evaluating('eNgLiShhabckasjdsa'));
+console.log(evaluating('ENGLIShhabckasjdsa'));
+console.log(evaluating('ENGLISHhabckasjdsa'));
